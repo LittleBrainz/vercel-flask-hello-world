47e90e47a17e3b183fd858dfa9f9f1445ce3b505
Stephen Turner <stephen@littlebrainz.com>
2023-05-17 16:32:03 +0100

    Prettied

--- api/index.py
+++ api/index.py
@@ -1,11 +1,19 @@
+# api/index.py
+
+# https://vercel.com/docs/concepts/functions/serverless-functions/runtimes/python
+
+
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return 'Hello, World!'
 
+
 @app.route('/about')
 def about():
-    return 'About'
+    return 'About!'
